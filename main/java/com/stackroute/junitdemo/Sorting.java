4627118bc608f9cf205dd4ec4dd536f83b9f1b81
VaddiBhargavi <bhargavivaddi999@gmail.com>
2019-07-04 09:02:15 +0530

    pe1 changes made

--- main/java/com/stackroute/junitdemo/Sorting.java
+++ main/java/com/stackroute/junitdemo/Sorting.java
@@ -1,61 +1,83 @@
 package com.stackroute.junitdemo;
 
 public class Sorting {
-    public class Result {
         String sortedNumber;
         String evenSum;
         boolean isSumAboveLimit;
-    }
-
-    public Result sort(int number)
-    {
-        int tempnumber = number;
-        number = Math.abs(number);
-        Result result = new Result();
-        int temp = number;
-        int count = 0;
         int sum = 0;
-        int sortednumber = 0;
-        while (number > 0) //to get array length and evenSum
+
+        public String sort(int number)
         {
-            int remainder = number%10;
-            count++;
-            number = number/10;
-            if(remainder % 2 ==0) {
-                sum = sum + remainder;
-            }
-        }
-// checks for the even digits sum
-        int intArr[]= new int[count];
-        for(int i=0; temp > 0; i++)
-        {
-            intArr[i] = temp%10;
-            temp = temp/10;
-        }
-//Swaps the digits in non-increasing order
-        for(int i=0; i < count; i++)
-        {
-            for(int j=i; j < count; j++)
+            int tempnumber = number;
+            number = Math.abs(number);
+            String result = new String();
+            int temp = number;
+            int count = 0;
+
+            int sortednumber = 0;
+//to get array length and evenSum
+            while (number > 0)
             {
-                if(intArr[i] < intArr[j])
+                int remainder = number%10;
+                count++;
+                number = number/10;
+                if(remainder % 2 ==0)
+                    sum = sum + remainder;
+            }
+            // checks for the even digits sum
+            int intArr[]= new int[count];
+            for(int i=0; temp > 0; i++)
+            {
+                intArr[i] = temp%10;
+                temp = temp/10;
+            }
+//Swaps the digits in non-increasing order
+            for(int i=0; i < count; i++)
+            {
+                for(int j=i; j < count; j++)
                 {
-                    temp = intArr[i];
-                    intArr[i] = intArr[j];
-                    intArr[j] = temp;
+                    if(intArr[i] < intArr[j])
+                    {
+                        temp = intArr[i];
+                        intArr[i] = intArr[j];
+                        intArr[j] = temp;
+                    }
                 }
             }
+
+            for(int i=0; i<count; i++)
+            {
+                sortednumber = sortednumber*10 + intArr[i];
+            }
+
+            if(tempnumber < 0)
+            {
+                sortednumber = -sortednumber;
+            }
+
+            result = "Sorted number in non-increasing order:"+sortednumber;
+
+//isSumAboveLimit = sum>15? true:false;
+            return result.trim();
         }
-        for(int i=0; i<count; i++)
-        {
-            sortednumber = sortednumber*10 + intArr[i];
+        public String evenSumCheck(int number){
+            sort(number);
+            int count=0;
+            int sum=0;String result="";
+            while (number > 0)
+            {
+                int remainder = number%10;
+                count++;
+                number = number/10;
+                if(remainder % 2 ==0)
+                    sum = sum + remainder;
+            }
+            result = "Sum of even numbers:"+sum;
+            return result;
         }
-        if(tempnumber < 0)
-        {
-            sortednumber = -sortednumber;
+        public boolean limitationCheck(int number){
+            evenSumCheck(number);
+            isSumAboveLimit= sum>15? true:false;
+            return isSumAboveLimit;
         }
-        result.sortedNumber = "Sorted number in non-increasing order:"+sortednumber;
-        result.evenSum = "Sum of even numbers:"+sum;
-        result.isSumAboveLimit = sum>15? true:false;
-        return result;
     }
-}
--- main/test/com/stackroute/junitdemo/SortingTest.java
+++ main/test/com/stackroute/junitdemo/SortingTest.java
@@ -2,59 +2,75 @@ package com.stackroute.junitdemo;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SortingTest {
     Sorting sorting;
-
     @Before
     public void setUp()
     {
-        System.out.println("Before");
+        System.out.println("before");
         sorting = new Sorting();
     }
 
     @After
     public void tearDown()
     {
-        System.out.println("After");
+        System.out.println("after");
         sorting = null;
     }
 
     @Test
     public void givenNumberShouldReturnSortedNumberAndSumBelowLimit()
     {
-        Sorting.Result result = sorting.sort(234534);
+        String result = sorting.sort(234534);
+        String result1 = sorting.evenSumCheck(234534);
+        boolean result3=sorting.limitationCheck(234534);
 
-        assertEquals("Sorted number in non-increasing order:"+544332,result.sortedNumber);
-        assertEquals("Sum of even numbers:"+10,result.evenSum);
-        assertEquals(false,result.isSumAboveLimit);
+        assertEquals("Sorted number in non-increasing order:"+ 544332,result);
+        assertEquals("Sum of even numbers:"+10, result1);
+        assertEquals(true,result3);
     }
+
     @Test
     public void givenNumberShouldReturnSortedNumberAndSumAboveLimit()
     {
-        Sorting.Result result = sorting.sort(2346158);
+        String result = sorting.sort(2346158);
+        String result1 = sorting.evenSumCheck(2346158);
+        boolean result3=sorting.limitationCheck(2346158);
 
-        assertEquals("Sorted number in non-increasing order:"+8654321, result.sortedNumber);
-        assertEquals("Sum of even numbers:"+20,result.evenSum);
-        assertEquals(true,result.isSumAboveLimit);
+
+        assertEquals("Sorted number in non-increasing order:"+8654321, result);
+        assertEquals("Sum of even numbers:"+20, result1);
+        assertEquals(true,result3);
     }
+
     @Test
     public void givenZeroShouldReturnSortedNumberAndSumAboveLimit()
     {
-        Sorting.Result result = sorting.sort(0);
+        String result = sorting.sort(0);
+        String result1 = sorting.evenSumCheck(0);
+        boolean result3=sorting.limitationCheck(0);
 
-        assertEquals("Sorted number in non-increasing order:"+0,result.sortedNumber);
-        assertEquals("Sum of even numbers:"+0,result.evenSum);
-        assertEquals(false,result.isSumAboveLimit);
+        assertEquals("Sorted number in non-increasing order:"+0,result);
+        assertEquals("Sum of even numbers:"+0, result1);
+        assertEquals(false,result3);
     }
+
     @Test
     public void givenNegativeShouldReturnSortedNumberAndSumAboveLimit()
     {
-        Sorting.Result result = sorting.sort(-921346);
+        String result = sorting.sort(-921346);
+        String result1 = sorting.evenSumCheck(921346);
+        boolean result3=sorting.limitationCheck(921346);
 
-        assertEquals("Sorted number in non-increasing order:"+-964321,result.sortedNumber);
-        assertEquals("Sum of even numbers:"+12,result.evenSum);
-        assertEquals(false,result.isSumAboveLimit);
+        assertEquals("Sorted number in non-increasing order:"+-964321, result);
+        assertEquals("Sum of even numbers:"+12, result1);
+        assertEquals(true,result3);
     }
+
+
+
+
 }
